2320ed4890066678b5ec8e385c66f672c38eefc2
abbsimoga <Simon.OgaardJozic@abbindustrigymnasium.se>
2021-06-28 16:00:38 +0200

    Update SpriteInitializer.cs

--- Demo/Assets/2D Sprites/SpriteInitializer.cs
+++ Demo/Assets/2D Sprites/SpriteInitializer.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 /// <summary>
 /// This script translates imported pixelart to worlds space.
-/// The Sprite Initializer component initializes gameobject with sprite renderer at an angle calculated to offset cameras isometric view.
-/// Alternatively this component stretches sprite in Y scale to offset cameras isometric view.
+/// The Sprite Initializer component initializes gameobject with a sprite renderer, scale in Y to offset cameras isometric view and local position to render sprite from ground level.
 /// Also works for Animations.
 /// Sprites must be set to:
 ///                         texture_type.Sprite
@@ -15,40 +14,10 @@ using UnityEngine;
 /// </summary>
 public class SpriteInitializer : MonoBehaviour
 {
-    // Sprite that gets rendered
     [SerializeField]
     private Sprite sprite_to_render;
     [SerializeField]
     private RuntimeAnimatorController animation_to_render;
-
-    /// <summary>
-    /// Initializes gameobject at an angle calculated to offset cameras isometric view
-    /// <summary>
-    private void InitializeRotated(Sprite sprite)
-    {
-        // Initializes gameobject as child with sprite renderer component and given sprite
-        GameObject game_object = new GameObject();
-        game_object.AddComponent<SpriteRenderer>();
-        game_object.GetComponent<SpriteRenderer>().sprite = sprite;
-        game_object.transform.parent = transform;
-
-        // Applies rotation to gameobject so it is facing the camera
-        Quaternion rotation = Quaternion.Euler(30f, 0f, 0f);
-        game_object.transform.localPosition = new Vector3(0f, 2.5f, 0f);
-        game_object.transform.rotation = rotation;
-    }
-    private void InitializeRotated(RuntimeAnimatorController animation)
-    {
-        GameObject game_object = new GameObject();
-        game_object.AddComponent<SpriteRenderer>();
-        game_object.AddComponent<Animator>();
-        game_object.GetComponent<Animator>().runtimeAnimatorController = animation;
-        game_object.transform.parent = transform;
-
-        Quaternion rotation = Quaternion.Euler(30f, 0f, 0f);
-        game_object.transform.localPosition = new Vector3(0f, 2.5f, 0f);
-        game_object.transform.rotation = rotation;
-    }
 
     /// <summary>
     /// Initializes gameobject stretched in Y scale to offset cameras isometric view
