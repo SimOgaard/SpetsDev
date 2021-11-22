afc578448bd08f2cb500c9d6d94faf7addb1a70c
SimOgaard <Zhyosgames@gmail.com>
2021-11-22 10:13:46 +0100

    forgot

--- Demo/Assets/UI/CanvasBillboard.cs
+++ Demo/Assets/UI/CanvasBillboard.cs
@@ -10,7 +10,7 @@ public class CanvasBillboard : MonoBehaviour
         main_camera = Camera.main;
 
         float one_div = 1f / PixelPerfectCameraRotation.pixels_per_unit;
-        transform.localScale = new Vector3(one_div, one_div * SpriteInitializer.y_scale, one_div);
+        transform.localScale = new Vector3(one_div / transform.lossyScale.x, one_div * SpriteInitializer.y_scale / transform.lossyScale.y, one_div / transform.lossyScale.z);
     }
 
     private void LateUpdate()
